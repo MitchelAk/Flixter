80a3eb5ada0e94c0bc96f89b53c1bdb4c33f7ab1
Mitchel Igolimah <mitcheligolimah@fb.com>
2022-06-16 14:12:34 -0700

    Setting up the table view

---
+++ Flixter/MovieViewController.h
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.h
+//  Flixter
+//
+//  Created by Mitchel Igolimah on 6/16/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
