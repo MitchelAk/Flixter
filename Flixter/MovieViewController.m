2aa281550f6f6d41fe52d9aaa14b6947a1ee78e1
Mitchel Igolimah <mitcheligolimah@fb.com>
2022-06-16 14:19:26 -0700

    Updated the tableView

--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -7,7 +7,10 @@
 
 #import "MovieViewController.h"
 
-@interface MovieViewController ()
+@interface MovieViewController () <UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (nonatomic, strong) NSArray *movies;
 
 @end
 
@@ -15,8 +18,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.dataSource = self;
+    
+    
+    
+    
     // Do any additional setup after loading the view.
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=043cf69f9597ab9a0a63b5c570ef7a7f"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               NSLog(@"%@", dataDictionary);
+               
+               
+               // TODO: Get the array of movies
+               self.movies = dataDictionary[@"results"];
+               for (NSDictionary *movie in self.movies) {
+               NSLog(@"%@", movie[@"title"]);
+               }
+               
+               // TODO: Store the movies in a property to use elsewhere
+               // TODO: Reload your table view data
+               [self.tableView reloadData];
+           }
+       }];
+    [task resume];
 }
+
+  - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+      return self.movies.count;
+      
+  }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    return cell;
+    
+}
+
+
+       
+
 
 /*
 #pragma mark - Navigation
@@ -27,5 +74,6 @@
     // Pass the selected object to the new view controller.
 }
 */
+
 
 @end
