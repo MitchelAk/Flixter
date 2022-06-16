9769d589f413cd5dfd27ed282f1a8216b53fcc2e
Mitchel Igolimah <mitcheligolimah@fb.com>
2022-06-16 13:33:18 -0700

    Initial Commit

---
+++ Flixter/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Flixter
+//
+//  Created by Mitchel Igolimah on 6/16/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
